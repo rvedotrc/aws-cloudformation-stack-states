4f5474a1412ea1948f3d595a65ffd401c20b031f
Rachel Evans <git@rve.org.uk>
2016-09-23 19:12:21 +0100

    create / update stack diagrams

--- cf-stack-states.sh
+++ cf-stack-states.sh
@@ -5,4 +5,6 @@ dot -Tpng -o output/cf.png ./cf-stack-states.dot
 dot -Tpng -o output/cf-happy-subset.png -Glayerselect=happy ./cf-stack-states.dot
 dot -Tpng -o output/cf-happy.png ./cf-happy.dot
 dot -Tpng -o output/cf-happy-atrest.png ./cf-happy-atrest.dot
+dot -Tpng -o output/create-stack.png ./create-stack.dot
+dot -Tpng -o output/update-stack.png ./update-stack.dot
 
